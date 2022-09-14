28a2f621e3230ca238d788f1a4fbe5528d804095
Arron Lacey <a.s.lacey@swansea.ac.uk>
2022-09-14 22:36:39 +0100

    Update run-car-plumber.R

--- run-car-plumber.R
+++ run-car-plumber.R
@@ -2,8 +2,12 @@ library(tidymodels)
 library(vetiver)
 library(plumber)
 
+
+# create tidymodels workflow 
 car_mod <-
     workflow(mpg ~ ., linear_reg()) %>%
     fit(mtcars)
+# store as vetiver model
 v <- vetiver_model(car_mod, "cars_mpg")
+# initialize plumber API serving the model
 pr() %>% vetiver_api(v) %>% pr_run(port = 8080)
